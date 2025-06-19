1d63e17ecfca75bcddb5afc85ef2f0df424c92b7
Nour Hagag <nourhgag159@gmail.com>
2025-06-19 22:13:48 +0300

    notes view body & app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          "Notes",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
