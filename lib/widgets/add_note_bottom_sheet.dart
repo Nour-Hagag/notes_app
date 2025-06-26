739dbe3d4c9fae9a26da7415b41484dc28bd0295
Nour Hagag <nourhgag159@gmail.com>
2025-06-26 15:51:44 +0300

    finish notes bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +9,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hint: "Title"),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'Content', maxLines: 6),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            const CustomTextField(hint: "Title"),
+            const SizedBox(height: 16),
+            const CustomTextField(hint: 'Content', maxLines: 5),
+            SizedBox(height: 30),
+            CustomAddButton(onTap: () {}),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomAddButton extends StatelessWidget {
+  const CustomAddButton({super.key, required this.onTap});
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 45,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
