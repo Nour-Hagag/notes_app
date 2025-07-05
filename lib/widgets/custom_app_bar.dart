c3d6eb0bbb2b9575864489bd935333ce6b771cca
Nour Hagag <nourhgag159@gmail.com>
2025-07-06 02:38:14 +0300

    add custom app bar to edit note view

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text(
-          "Notes",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         ),
-        CustomSearchIcon(),
+        CustomIcon(icon: icon),
       ],
     );
   }
